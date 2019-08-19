99efcd4b2879af7aa673a551aee2bd5dad55385e
Ray Yao <rocaray51@gmail.com>
2019-08-19 04:20:21 -0700

    Updates

---
+++ precache-manifest.242ad720db8d8b476ae2ba954bfe7f4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a885bf96388835983749af6b73ccbe84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5fd2d8775c8fb879239",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b49e7181e1df7849b273",
+    "url": "/static/js/2.2f7898c6.chunk.js"
+  },
+  {
+    "revision": "b5fd2d8775c8fb879239",
+    "url": "/static/js/main.9b3d9b4f.chunk.js"
+  },
+  {
+    "revision": "2e87bcf017c1e01fd1d7",
+    "url": "/static/js/runtime~main.63ff2b0f.js"
+  }
+]);
